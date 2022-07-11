d24b3879119e5161c5bfec153e84c16f4d2a01a5
Vlas <willi86@mail.ru>
2022-07-11 17:46:33 +0300

    add hwTask004

---
+++ hwTask004/Program.cs
@@ -0,0 +1,16 @@
+﻿// Домашка Задача 8: Напишите программу, 
+// которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count <= number)
+{
+    if (count % 2 == 0)
+    {
+       int evenNumber = count;
+       Console.WriteLine(evenNumber);
+    }
+count++;
+}
