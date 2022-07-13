716aa9eefcfc7636d696cfdea65ca93da86fc6c4
Vlas <willi86@mail.ru>
2022-07-13 03:30:46 +0300

    add hwTask201 Задача10

---
+++ hwTask201/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if( number > 99 && number < 1000)
+{
+    int x1 = number % 100;
+    x1 /= 10;
+    Console.WriteLine(x1);
+}
+else
+{
+    Console.WriteLine("Не обманывай меня!");
+}
+
+// Второй вариант решения
+/*
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if ( number > 99 && number < 1000)
+{
+    string num = number.ToString();
+    Console.WriteLine($"Вторая цифра: {num[1]}");
+}
+else
+{
+    Console.WriteLine("Не лги себе");
+}
+*/
