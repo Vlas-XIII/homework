f177255080fdb072d3d4e8c448a4600734a2e5fd
Vlas <willi86@mail.ru>
2022-07-13 19:11:04 +0300

    Добавил вариант с отрицательным значением

--- hwTask201/Program.cs
+++ hwTask201/Program.cs
@@ -4,31 +4,43 @@
 // 782 -> 8
 // 918 -> 1
 
-
+/*
 Console.Write("Введите трёхзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if( number > 99 && number < 1000)
+if (number > 99 && number < 1000)
 {
     int x1 = number % 100;
     x1 /= 10;
+    Console.WriteLine("Вторая цифра: " + x1);
+}
+else if (number < -99 && number > -1000)
+{
+    int x1 = number % 100;
+    x1 /= 10;
+    x1 *= -1;
     Console.WriteLine("Вторая цифра: " + x1);
 }
 else
 {
     Console.WriteLine("Не лги себе");
 }
+*/
 
 // Второй вариант решения
-/*
+
 Console.Write("Введите трёхзначное число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-if ( number > 99 && number < 1000)
+if (number > 99 && number < 1000)
 {
     string num = number.ToString();
     Console.WriteLine($"Вторая цифра: {num[1]}");
+}
+else if (number < -99 && number > -1000)
+{
+    string num = number.ToString();
+    Console.WriteLine($"Вторая цифра: {num[2]}");
 }
 else
 {
     Console.WriteLine("Не лги себе");
 }
-*/
