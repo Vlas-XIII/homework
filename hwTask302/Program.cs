7862e4bf0bf8eb448f3d391a31554c8fc3f39873
Vlas <willi86@mail.ru>
2022-07-19 02:34:59 +0300

    add hwTask302 Задача 21

---
+++ hwTask302/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек 
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+// Формула: l = √ (x 2 - x 1)^2 + (y 2 - y 1)^2 + (z 2 - z 1)^2
+
+
+Console.Write($"Введите координату A (x,y,z): ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату B (x,y,z): ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+var result = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+Console.WriteLine(result);
